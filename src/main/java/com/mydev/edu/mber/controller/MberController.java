8eac222410e0157a5420834c0088c57f619dca31
Jungpal-Note <Jungpal-Note@Jungpal-Note>
2022-03-30 21:57:08 +0900

    # 2022.03.30
    1. 회원가입시 가입완료 이메일 로직 추가(gmail)

--- src/main/java/com/mydev/edu/mber/controller/MberController.java
+++ src/main/java/com/mydev/edu/mber/controller/MberController.java
@@ -1,8 +1,17 @@
 package com.mydev.edu.mber.controller;
 
 import java.util.List;
+import java.util.Properties;
 
 import javax.annotation.Resource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -105,6 +114,9 @@ public class MberController {
 		
 		if (totCnt > 0) { // 성공
 			
+			// 회원가입 메일발송
+			gmailSend(mberVO.getMberId());
+			
 			mav.addObject("resultCode", "0");
 			mav.addObject("resultMsg", "회원가입이 되었습니다.\n로그인 페이지로 이동합니다.");
 		} else { // 실패
@@ -114,6 +126,59 @@ public class MberController {
 
 		mav.setViewName("jsonView");
 		return mav;
+	}
+	
+	/**
+	 * 메일발송(Gmail)
+	 */
+	public static void gmailSend(String regId) {
+		// Gmail 계정
+		String mail_id = "sinjie111@gmail.com";
+		// Gmail 패스워드
+		String mail_pw = "sfayduamftbaswcg";
+		
+		// 회원가입시 작성한 ID
+		String getRegId = regId;
+		
+		// SMTP 서버정보 설정
+		Properties prop = new Properties();
+		prop.put("mail.smtp.host", "smtp.gmail.com");
+		prop.put("mail.smtp.port", "465");
+		prop.put("mail.smtp.auth", "true");
+		prop.put("mail.smtp.ssl.enable", "true");
+		prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		
+		Session session = Session.getDefaultInstance(prop, new javax.mail.Authenticator() { 
+			protected PasswordAuthentication getPasswordAuthentication() { 
+				return new PasswordAuthentication(mail_id, mail_pw); 
+			} 
+		});
+		
+		try {
+			
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(mail_id));
+			
+			// 수신자 메일주소
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(mail_id));
+			
+			// 메일 제목
+			message.setSubject("회원가입 메일 입니다.");
+			
+			// 메일 내용
+			message.setText("회원ID: " + getRegId + "\n회원가입이 완료 되었습니다.\n로그인을 진행해 주세요.");
+			
+			// 메일 전송
+			Transport.send(message);
+			
+			System.out.println("message send Success.....");
+
+		} catch (AddressException e) {
+			e.printStackTrace();
+		} catch (MessagingException ex) {
+			ex.printStackTrace();
+		}
+		
 	}
 	
 }
